12321e2d990aae8bc282ee02c3c5d0d111d21e4d
Pedro Tovar <petovar@gmail.com>
2026-05-02 07:36:08 -0400

    ...

--- lib/src/providers/theme_provider.dart
+++ lib/src/providers/theme_provider.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider with ChangeNotifier {
+  static const String _themeKey = 'theme_mode';
   ThemeMode _themeMode = ThemeMode.light;
+
+  ThemeProvider() {
+    _loadTheme();
+  }
 
   ThemeMode get themeMode => _themeMode;
 
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isDark = prefs.getBool(_themeKey) ?? false;
+    _themeMode = isDark ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+  }
+
   void toggleTheme() {
     _themeMode = (_themeMode == ThemeMode.dark) ? ThemeMode.light : ThemeMode.dark;
+    _saveTheme();
     notifyListeners();
   }
 
   void setTheme(ThemeMode mode) {
     if (_themeMode != mode) {
       _themeMode = mode;
+      _saveTheme();
       notifyListeners();
     }
+  }
+
+  Future<void> _saveTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_themeKey, _themeMode == ThemeMode.dark);
   }
 }
